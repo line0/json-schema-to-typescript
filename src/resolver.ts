a55d5bab6bc8203035f31f551e5dcbd896435d2b
Sebastian Matthes <sebastian.matthes@volkswagen.de>
2024-01-28 19:38:53 +0100

    use latest upstream `@apidevtools/json-schema-ref-parser` instead of outdated fork
    requires node >= 16

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,12 +1,15 @@
-import $RefParser = require('@bcherny/json-schema-ref-parser')
+import $RefParser, {ParserOptions} from '@apidevtools/json-schema-ref-parser'
 import {JSONSchema} from './types/JSONSchema'
 import {log} from './utils'
 
-export type DereferencedPaths = WeakMap<$RefParser.JSONSchemaObject, string>
+import type {JSONSchema4Object, JSONSchema6Object, JSONSchema7Object} from 'json-schema'
+
+export type JSONSchemaObject = JSONSchema4Object | JSONSchema6Object | JSONSchema7Object
+export type DereferencedPaths = WeakMap<JSONSchemaObject, string>
 
 export async function dereference(
   schema: JSONSchema,
-  {cwd, $refOptions}: {cwd: string; $refOptions: $RefParser.Options},
+  {cwd, $refOptions}: {cwd: string; $refOptions: ParserOptions},
 ): Promise<{dereferencedPaths: DereferencedPaths; dereferencedSchema: JSONSchema}> {
   log('green', 'dereferencer', 'Dereferencing input schema:', cwd, schema)
   const parser = new $RefParser()
@@ -15,8 +18,8 @@ export async function dereference(
     ...$refOptions,
     dereference: {
       ...$refOptions.dereference,
-      onDereference($ref, schema) {
-        dereferencedPaths.set(schema, $ref)
+      onDereference(path: string, value: JSONSchemaObject) {
+        dereferencedPaths.set(value, path)
       },
     },
   })) as any // TODO: fix types
